be6bbc9d1ea5922e3a9f3f4cc16d9e0f3e432e9b
mimoex <nijidream@protonmail.com>
2023-10-06 10:17:35 +0900

    Add check flag; Fix time_move_down();

--- looopdenki.py
+++ looopdenki.py
@@ -3,6 +3,7 @@ import sys
 import json
 from datetime import datetime, timedelta
 import subprocess
+import time
 
 def get_prices(url):
     # GETリクエストを送信してJSONデータを取得
@@ -21,18 +22,18 @@ def run_subprocess():
     # ここに実行したいコマンドを記述
     subprocess.run(["your_command_here"])
 
-def time_move_up():
+def time_move_down():
     # 現在の時刻を取得
     current_time = datetime.now()
 
     # 現在の分数を取得
     current_minute = current_time.minute
 
-    # 30分ごとに切り上げる
+    # 30分ごとに切り下げ
     if current_minute >= 30:
-        current_time += timedelta(minutes=(60 - current_minute))
+        current_time -= timedelta(minutes=(current_minute - 30))
     else:
-        current_time += timedelta(minutes=(30 - current_minute))
+        current_time -= timedelta(minutes=current_minute)
 
     # 新しい時刻をHHMM形式で表示
     new_hhmm = current_time.strftime('%H%M')
@@ -45,6 +46,7 @@ def get_flag_at_time(data_list, input_time):
             return flag
     return None  # 時刻が見つからない場合はNoneを返すか、適切なデフォルト値を返すことができます
 
+# APIからでんき日和を取得し、そうではなくなる時間を取得する関数
 def get_now_denkibiyori():
     # APIエンドポイントのURLを指定
     api_url = "https://looop-denki.com/api/prices?select_area=01"
@@ -69,43 +71,55 @@ def get_now_denkibiyori():
 
     merged_list = [(hhmm_list[i], level_1[i]) for i in range(len(level_1))]
 
-    # テストデータ
-    #merged_list = [('0000', 0), ('0030', 0), ('0100', 0), ('0130', 0), ('0200', 0), ('0230', 0), ('0300', 0), ('0330', 0), ('0400', 0), ('0430', 0), ('0500', 0), ('0530', 0), ('0600', 0), ('0630', 0), ('0700', 0), ('0730', 0), ('0800', 0), ('0830', -0.5), ('0900', -0.5), ('0930', -0.5), ('1000', -0.5), ('1030', -0.5), ('1100', -0.5), ('1130', -0.5), ('1200', -0.5), ('1230', -0.5), ('1300', -0.5), ('1330', 0), ('1400', 0), ('1430', 0), ('1500', 0), ('1530', 0), ('1600', 0.5), ('1630', 0.5), ('1700', 0.5), ('1730', 0.5), ('1800', 0.5), ('1830', 0.5), ('1900', 0.5), ('1930', 0), ('2000', 0), ('2030', 0), ('2100', 0.5), ('2130', 0), ('2200', 9), ('2230', 0), ('2300', 0), ('2330', 0)]
+    # データが-0.5から0になる時間（HHMM形式）を取得
+    biyori_endtime = None
+    for i in range(len(merged_list) - 1):
+        if merged_list[i][1] != -0.5 and merged_list[i + 1][1] == -0.5:
+            biyori_endtime = merged_list[i][0]
+            break
 
-    near_time=time_move_up()
+    if biyori_endtime:
+        print(f"でんき日和終了時刻は {biyori_endtime} です。")
+    else:
+        print("該当する時間はありません。")
+        sys.exit(1) # プログラムを終了
+
+    # テストデータ
+    #merged_list = [
+    # ('0000', 0), ('0030', 0), ('0100', 0), ('0130', 0), ('0200', 0), ('0230', 0), ('0300', 0), ('0330', 0), ('0400', 0), ('0430', 0), 
+    # ('0500', 0), ('0530', 0), ('0600', 0), ('0630', 0), ('0700', 0), ('0730', 0), ('0800', 0), ('0830', -0.5), ('0900', -0.5), ('0930', -0.5), 
+    # ('1000', -0.5), ('1030', -0.5), ('1100', -0.5), ('1130', -0.5), ('1200', -0.5), ('1230', -0.5), ('1300', -0.5), ('1330', 0), ('1400', 0), 
+    # ('1430', 0), ('1500', 0), ('1530', 0), ('1600', 0.5), ('1630', 0.5), ('1700', 0.5), ('1730', 0.5), ('1800', 0.5), ('1830', 0.5), ('1900', 0.5), 
+    # ('1930', 0), ('2000', 0), ('2030', 0), ('2100', 0.5), ('2130', 0), ('2200', 9), ('2230', 0), ('2300', 0), ('2330', 0)
+    # ]
+
+    near_time=time_move_down()
 
     denki_biyori=get_flag_at_time(merged_list, near_time)
-    return denki_biyori
+    return denki_biyori, biyori_endtime
 
+# 採掘開始 (end_time: 'HHMM')
+def start_mining(end_time):
+    try:
+        xmrig_process = subprocess.Popen(["path/to/xmrig"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print("マイニングを開始しました。")
+        print(f"{end_time}に終了します。")
 
-import time
+        while True:
+            current_time = time.strftime("%H%M")
 
-# マイニングプログラムを実行するコマンド
-command = ["sh", "/home/user/mining/lolminer1.76/1nicehash_nexa.sh"]
+            # 終了時間に達したらxmrigを終了
+            if current_time >= end_time:
+                xmrig_process.terminate()
+                print("マイニングを終了しました。")
+                break
 
-# サブプロセスを開始
-process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            # 一定時間（例: 1分）待機して再度時刻を確認
+            time.sleep(60)
 
-# 20分ごとにフラグの値を調べて終了するかどうかを確認
-while True:
-    # サブプロセスの状態を調べる
-    return_code = process.poll()
-    if return_code is not None:
-        # サブプロセスが終了している場合
-        print(f"サブプロセスが終了しました。終了コード: {return_code}")
-        break
+    except Exception as e:
+        print(f"エラーが発生しました: {str(e)}")
 
-    # 20分待つ
-    time.sleep(5)  # 20分 = 1200秒
-
-    # フラグの値を調べる（仮のコード、実際のコードに合わせて変更が必要）
-    flag_value = get_now_denkibiyori()  # フラグの値を取得する関数を定義する必要があります
-
-    if flag_value != -0.5:
-        # フラグの値が-0.5以外の場合、サブプロセスを終了する
-        print(f"フラグの値が-0.5以外 ({flag_value}) なので、サブプロセスを終了します。")
-        process.terminate()  # サブプロセスを終了させる
-
-# サブプロセスの終了を待つ
-process.wait()
-print("プログラムが完了しました。")
+if __name__ == "__main__":
+    flag, end_time=get_now_denkibiyori()
+    start_mining(end_time)
